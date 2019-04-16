6eea089447a063143bd09df61ec4b6c1e50cf971
Jonathan Ni <jrn73@cornell.edu>
2019-04-15 22:34:32 -0400

    Added US Market News

--- news_crawler.py
+++ news_crawler.py
@@ -1,10 +1,13 @@
+# -*- coding: utf-8 -*-
+
 # import libraries
 import requests
 from bs4 import BeautifulSoup
 import datetime
 import pandas as pd
-
 import sys
+import tqdm
+import time
 
 def get_dates(start, end):
     dates = []
@@ -23,47 +26,71 @@ def article_time_scrap(url):
 	page = requests.get(url)
 	soup = BeautifulSoup(page.content, 'html.parser')
 	
-	dt = soup.find('div', class_='ArticleHeader_date').text
-	return dt.split('/')[1].strip()
+	dt = soup.find('div', class_='ArticleHeader_date')
+	if dt is None: dt = soup.find('div', class_='HeroArticleHeader_date')
 	
-def scrap(url):
+	return dt.text.split('/')[1].strip()
+
+def scrap(url, remove_el='div', remove_class='actionButton', 
+  text_class=lambda s: s in ["feature", "topStory"]):
     # query the website and return the html to the variable ‘page’
-    articles = []
     page = requests.get(url)
 
     # parse the html using beautiful soup and store in variable `soup`
     soup = BeautifulSoup(page.content, 'html.parser')
 
-	# Remove action buttons
-    for i in soup.findAll('div', class_='actionButton'): i.extract()
+    for i in soup.findAll(remove_el, class_=remove_class): i.extract()
 	
     # Take out the <div> of all articles and get its value
-    text_box = soup.findAll('div', class_=lambda s: s in ["feature", "topStory"])
+    text_box = soup.findAll('div', class_=text_class)[::-1]
+	
     for t in text_box:
         link = t.a['href']
-        time = article_time_scrap(f"https://www.reuters.com{link}")
 		
-        text = t.text.strip()
-        articles.append((time, text))
-    return articles
+        if link in ["/article/lendingclub-website/corrected-lending-club-website-goes-down-cites-data-center-outage-idUSL1N19100U"]: 
+            continue
+		
+        flag = True
+        while flag:
+            flag = False
+            try:
+                hhmm = article_time_scrap(f"https://www.reuters.com{link}")
+            except AttributeError:
+                print("\n\n**"+url+"    "+str(t))
+                print(f"https://www.reuters.com{link} **\n\n")
+                flag = True
+                print("Waiting for 60s...")
+                time.sleep(60)
 
+        text = t.text.strip().replace("\u200b", "").replace("\u200d", "").replace("\uff0c", ", ")
+        yield (hhmm, text)
 
-companies = ['GS.A'] #['AMZN.OQ', 'FB.OQ', 'TSLA.OQ', 'GE.A', 'GS.A']
-start = datetime.datetime.strptime("03-03-2014", "%m-%d-%Y")
-end = datetime.datetime.strptime("03-02-2019", "%m-%d-%Y")
-dates = get_dates(start, end)
-
-for company in companies:
+def process_date(date):
     df = pd.DataFrame(columns=['date', 'time', 'title', 'abstract'])
-    for date in dates:
-        print(date)
-        url = f"https://www.reuters.com/finance/stocks/company-news/{company}?date={date}"
-        articles = scrap(url)
-        if articles:
-            for time, article in articles:
-                title, abstract = get_title_abstrct(article)
-                new_date = date[4:] + '-' + date[:2] + '-' + date[2:4]
-                new_row = pd.Series([new_date, time, title, abstract], index=['date', 'time', 'title', 'abstract'])
-                df = pd.concat([df, new_row.to_frame().T])
-    df.to_csv('News/'+company+'.csv', index = False)
 
+    #url = f"https://www.reuters.com/finance/stocks/company-news/{company}?date={date}"
+    url = f"https://www.reuters.com/finance/markets/us?date={date}"
+    articles = scrap(url, remove_el='time', remove_class='article-time', text_class='story-content')
+    if articles:
+        for time, article in articles:
+            title, abstract = get_title_abstrct(article)
+            new_date = date[4:] + '-' + date[:2] + '-' + date[2:4]
+            new_row = pd.Series([new_date, time, title, abstract], index=['date', 'time', 'title', 'abstract'])
+            #print(new_row.to_frame().T.to_csv(index=False))
+            df = pd.concat([df, new_row.to_frame().T])
+	
+    return df
+
+if __name__ == '__main__':
+    company = 'US' #['AMZN.OQ', 'FB.OQ', 'TSLA.OQ', 'GE.A', 'GS.A']
+    #start = datetime.datetime.strptime("03-03-2014", "%m-%d-%Y")
+    start = datetime.datetime.strptime("02-05-2018", "%m-%d-%Y")
+    end = datetime.datetime.strptime("03-02-2019", "%m-%d-%Y")
+    dates = get_dates(start, end)
+	
+    f = open(f"News/{company}.csv", "a+")
+	
+    #print("date,time,title,abstract", file=f)
+	
+    for d in tqdm.tqdm(map(process_date, dates), total=len(dates)):
+        print(d.to_csv(index=False), file=f)
