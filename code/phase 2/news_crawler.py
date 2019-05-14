ea9ac8a3d6f29723f22eb594f860f7c1c24ce87f
Lily Liu <xl379@cornell.edu>
2019-05-14 14:09:39 -0400

    DONE

---
+++ code/phase 2/news_crawler.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+# import libraries
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import pandas as pd
+import sys
+import tqdm
+import time
+
+def get_dates(start, end):
+    dates = []
+    date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
+    for date in date_generated:
+        dates.append(date.strftime("%m%d%Y"))
+    return dates
+
+def get_title_abstrct(text):
+    t = text.splitlines()
+    title = t[0]
+    abstract = " ".join(t[1:])
+    return title, abstract
+
+def article_time_scrap(url):
+	page = requests.get(url)
+	soup = BeautifulSoup(page.content, 'html.parser')
+	
+	dt = soup.find('div', class_='ArticleHeader_date')
+	if dt is None: dt = soup.find('div', class_='HeroArticleHeader_date')
+	
+	return dt.text.split('/')[1].strip()
+
+def scrap(url, remove_el='div', remove_class='actionButton', 
+  text_class=lambda s: s in ["feature", "topStory"]):
+    # query the website and return the html to the variable ‘page’
+    page = requests.get(url)
+
+    # parse the html using beautiful soup and store in variable `soup`
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    for i in soup.findAll(remove_el, class_=remove_class): i.extract()
+	
+    # Take out the <div> of all articles and get its value
+    text_box = soup.findAll('div', class_=text_class)[::-1]
+	
+    for t in text_box:
+        link = t.a['href']
+		
+        if link in ["/article/lendingclub-website/corrected-lending-club-website-goes-down-cites-data-center-outage-idUSL1N19100U"]: 
+            continue
+		
+        flag = True
+        while flag:
+            flag = False
+            try:
+                hhmm = article_time_scrap(f"https://www.reuters.com{link}")
+            except AttributeError:
+                print("\n\n**"+url+"    "+str(t))
+                print(f"https://www.reuters.com{link} **\n\n")
+                flag = True
+                print("Waiting for 60s...")
+                time.sleep(60)
+
+        text = t.text.strip().replace("\u200b", "").replace("\u200d", "").replace("\uff0c", ", ")
+        yield (hhmm, text)
+
+def process_date(date):
+    df = pd.DataFrame(columns=['date', 'time', 'title', 'abstract'])
+
+    url = f"https://www.reuters.com/finance/stocks/company-news/{company}?date={date}"
+    #url = f"https://www.reuters.com/finance/markets/us?date={date}"
+    #articles = scrap(url, remove_el='time', remove_class='article-time', text_class='story-content')
+    articles = scrap(url)
+    if articles:
+        for time, article in articles:
+            title, abstract = get_title_abstrct(article)
+            new_date = date[4:] + '-' + date[:2] + '-' + date[2:4]
+            new_row = pd.Series([new_date, time, title, abstract], index=['date', 'time', 'title', 'abstract'])
+            #print(new_row.to_frame().T.to_csv(index=False))
+            df = pd.concat([df, new_row.to_frame().T])
+	
+    return df
+
+if __name__ == '__main__':
+    company = 'BA' #['AMZN.OQ', 'FB.OQ', 'TSLA.OQ', 'GE.A', 'GS.A']
+    #start = datetime.datetime.strptime("03-03-2014", "%m-%d-%Y")
+    start = datetime.datetime.strptime("01-01-2013", "%m-%d-%Y")
+    end = datetime.datetime.strptime("11-28-2017", "%m-%d-%Y")
+    dates = get_dates(start, end)
+	
+    f = open(f"News/{company}.csv", "a+")
+	
+    print("date,time,title,abstract", file=f)
+	
+    for d in tqdm.tqdm(map(process_date, dates), total=len(dates)):
+        print(d.to_csv(index=False, header=False), file=f, end="")
---
+++ code/phase 2/price_delta.py
@@ -0,0 +1,61 @@
+from aiat_utils import tryread, errexit
+import csv
+import os
+import sys
+
+def conv_row(row, conv_dict):
+    new_row = row
+    for i in conv_dict: new_row[i] = conv_dict[i](new_row[i])
+    return new_row
+
+def conv(data, conv_dict):
+    return map(lambda r: conv_row(r, conv_dict), data)
+
+def price_delta(data, date_col_n="history_day_date",
+                open_col_n="history_day_open",
+                close_col_n="history_day_close"):
+    if data is None or len(data) == 0:
+        raise ValueError("Data empty")
+    
+    cols = data[0]
+
+    if not set([date_col_n, open_col_n, close_col_n]).issubset(set(cols)):
+        raise ValueError("Data has incorrect columns")
+
+    date_col = cols.index(date_col_n)
+    open_col, close_col = cols.index(open_col_n), cols.index(close_col_n)
+
+    data = list(conv(data[1:], {open_col: float, close_col: float}))
+
+    calc_data = [date_col_n, "intra", "delta_pc"]
+    for i in range(len(data) - 1):
+        r_now, r_next = data[i], data[i + 1]
+        pc_intra = (r_now[close_col] - r_now[open_col]) / r_now[open_col]
+        pc_before = (r_next[open_col] - r_now[close_col]) / r_now[close_col]
+
+        calc_data.append([r_now[date_col], 1, pc_intra])
+        calc_data.append([r_next[date_col], 0, pc_before])
+
+    r_last = data[-1]
+    pc_intra_last = (r_last[close_col] - r_last[open_col]) / r_last[open_col]
+    calc_data.append([r_last[date_col], 1, pc_intra_last])
+
+    return calc_data
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        errexit("Usage: price_delta.py [data.csv]", 1)
+
+    data = tryread(sys.argv[1])
+
+    calc_data = None
+
+    try:
+        calc_data = price_delta(data)
+    except ValueError as e:
+        errexit(str(e), -2)
+
+    if not calc_data is None:
+        writer = csv.writer(sys.stdout)
+        writer.writerow(["history_day_date", "intra", "delta_pc"])
+        for r in calc_data: writer.writerow(r)
